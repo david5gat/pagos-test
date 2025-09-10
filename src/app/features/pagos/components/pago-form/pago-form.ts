8fd9f8f381cdf36e4b10ef4a8389261b78ce34cb
david5cat <david54gt@gmail.com>
2025-09-10 13:36:42 -0500

    todo instalado correctamente

---
+++ src/app/features/pagos/components/pago-form/pago-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pago-form',
+  imports: [],
+  templateUrl: './pago-form.html',
+  styleUrl: './pago-form.css'
+})
+export class PagoForm {
+
+}
---
+++ src/app/features/pagos/pages/pagos-list/pagos-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagos-list',
+  imports: [],
+  templateUrl: './pagos-list.html',
+  styleUrl: './pagos-list.css'
+})
+export class PagosList {
+
+}
